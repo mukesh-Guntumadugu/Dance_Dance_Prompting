c4360f3359d85c51e714da11e5edc1d982f04195
mukesh guntumadugu <66353214+mukesh-Guntumadugu@users.noreply.github.com>
2026-06-03 10:41:03 -0400

    script and songs for they bpm and onsets generted to over come librosa MIR errors and test case dd00dsd21

--- synthetic_dataset/submit_all_evals.sh
+++ synthetic_dataset/submit_all_evals.sh
@@ -5,9 +5,9 @@ scancel 32142
 
 echo "Submitting 5 individual parallel jobs..."
 sbatch --nodelist=node001 slurm_eval_librosa.sh
-sbatch --nodelist=node002 slurm_eval_qwen.sh
-sbatch --nodelist=node003 slurm_eval_mumu.sh
-sbatch --nodelist=node005 slurm_eval_flamingo.sh
-sbatch --nodelist=node006 slurm_eval_deepresonance.sh
+sbatch --nodelist=node004 slurm_eval_qwen.sh
+sbatch --nodelist=node007 slurm_eval_mumu.sh
+sbatch --nodelist=node008 slurm_eval_flamingo.sh
+sbatch --nodelist=node009 slurm_eval_deepresonance.sh
 
 echo "All jobs submitted in parallel!"
